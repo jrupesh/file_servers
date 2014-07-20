96fb90050cb78ca0a2e14ae31b113c874769f35f
Rupesh J <j.rupesh@gmail.com>
2014-07-20 18:33:02 +0530

    Test helper commented fixture.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
-ActiveSupport::TestCase.fixture_path=(File.expand_path(File.dirname(__FILE__)) + "/fixtures")
+# ActiveSupport::TestCase.fixture_path=(File.expand_path(File.dirname(__FILE__)) + "/fixtures")
 # ActiveSupport::TestCase.fixtures :all
